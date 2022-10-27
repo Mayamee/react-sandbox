91f64eacbac1ae14d155e900d6d04ed44de4d589
Mayamee <ponyashcat228@gmail.com>
2022-10-27 17:41:44 +0300

    useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,58 @@
-import { useEffect, useRef, useState } from "react";
+import { useMemo, useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const [text, setText] = useState("");
-  const renderCount = useRef(0);
-  const prevName = useRef("");
+  const [number, setNumber] = useState("");
+  const [darkMode, setDarkMode] = useState(false);
 
-  const handleChange = (e) => {
-    setText(e.target.value);
+  const doubleNumber = useMemo(() => slow(number), [number]);
+
+  const themeStyles = {
+    backgroundColor: darkMode ? "#333" : "#CCC",
+    color: darkMode ? "#CCC" : "#333",
+    padding: "2rem",
+    margin: "2rem",
   };
 
-  useEffect(() => {
-    renderCount.current += 1;
-  });
-
-  useEffect(() => {
-    prevName.current = text;
-  }, [text]);
-
+  const handleChange = (e) => {
+    setNumber(e.target.value);
+  };
   return (
-    <div>
-      <input
-        type="text"
-        name="name"
-        id="id"
-        value={text}
-        onChange={handleChange}
-      />
-      <h1>{text}</h1>
-      <h2>Rendered: {renderCount.current} times</h2>
-      <h3>
-        My text is {text} and it used to be {prevName.current}
-      </h3>
+    <div className="App">
+      <h1>My App</h1>
+      <div className="box" style={themeStyles}>
+        <input
+          type="number"
+          value={number}
+          placeholder="some text"
+          onChange={handleChange}
+        />
+        <div
+          className="result"
+          style={{
+            display: "inline-block",
+          }}
+        >
+          Result is: {doubleNumber}
+        </div>
+
+        <button
+          style={{
+            display: "block",
+            marginTop: "1rem",
+          }}
+          onClick={() => setDarkMode((prevMode) => !prevMode)}
+        >
+          Toggle Mode
+        </button>
+      </div>
     </div>
   );
 };
-
 export default App;
+
+function slow(number) {
+  console.log("Calling slow function");
+  for (let i = 0; i < 2; i++) for (let i = 0; i <= 5e8; i++) {}
+  return number * 2;
+}
